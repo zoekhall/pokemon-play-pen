4ac77c8237a38b96cef1268972e2deecbeca3ed2
codingMustache <jorcar1986@gmail.com>
2022-11-04 12:21:50 -0500

    SETUP: sever -its listening

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { config } = require('webpack');
 
+
 const app = express();
-const PORT = config.env.PORT || 8080;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));

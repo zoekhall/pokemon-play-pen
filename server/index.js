9224cdf4cbdfb482f5b43ed83aaff1cbbfb6d7cc
codingMustache <jorcar1986@gmail.com>
2022-11-04 12:18:37 -0500

    SETUP: server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-const express = require('express')
+const express = require('express');
+const { config } = require('webpack');
+
+const app = express();
+const PORT = config.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.listen(PORT, () => console.log(`Listening at localhost:${PORT}`) );

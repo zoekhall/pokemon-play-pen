9f33891f2f65564c2672457b8da5c6609a828f27
zachmarullo <zachmarullo@gmail.com>
2022-11-05 16:32:05 -0500

    Added some functionality to Google oAuth20

--- server/app.js
+++ server/app.js
@@ -1,16 +1,66 @@
 const path = require('path');
 const express = require('express');
-
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const clientID = '594164333356-4mh7nfte8udmjtlduk11vviq67ckmlip.apps.googleusercontent.com';
+const clientSecret = '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';
 const { User } = require('./routes/userRoutes.js');
 const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
-const app = express();
+const User1 = require('./index.js');
 
-app.use(express.urlencoded({extended: true}));
+
+const app = express();
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(CLIENT_PATH));
+// app.use(express.static(CLIENT_PATH));
+
+
+
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google"> Authenticate with Google</a>');
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure',
+  })
+);
+
+app.get('/protected', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/auth/failure', (req, res) => {
+  res.send('Something went wrong');
+});
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: 'http://localhost:8080' }),
+//   function (req, res) {
+//     res.redirect('http://localhost:8080');
+//   });
+
+
+app.get('/logout', function (req, res) {
+  res.redirect('http://localhost:8080/');
+});
 
 app.use('/api/user', User);
-
 
 
 module.exports = {
--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const clientID = '594164333356-4mh7nfte8udmjtlduk11vviq67ckmlip.apps.googleusercontent.com';
+const clientSecret = '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';
+
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const clientID = '594164333356-4mh7nfte8udmjtlduk11vviq67ckmlip.apps.googleusercontent.com';
+// const clientSecret = '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';
+const session = require('express-session');
+
 
 const mongoUri = 'mongodb://localhost/poke';
+
 
 mongoose.connect(mongoUri)
   .then(() => console.log('connected to mongodb'))
   .catch(err => console.log('you did not connect to mongodb'));
+
+
 
 const userSchema = new Schema({
   username: {
@@ -17,13 +32,17 @@ const userSchema = new Schema({
     type: String,
     require: true,
   },
+  googleId: String,
+  secret: String,
   deckId: String,
   favPokemon: String,
-  salt: String
 });
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-const chatSchema = new Schema ({
-  reciever: {
+
+const chatSchema = new Schema({
+  receiver: {
     type: String,
     require: true,
   },
@@ -36,25 +55,49 @@ const chatSchema = new Schema ({
 });
 
 const deckSchema = new Schema({
-  cardId: String, 
+  cardId: String,
   userId: String
 });
 
+passport.use(new GoogleStrategy({
+  clientID: clientID,
+  clientSecret: clientSecret,
+  callbackURL: 'http://localhost:8080/auth/google/callback',
+},
+function (accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 const deckModel = mongoose.model('Deck', deckSchema);
-const userModel = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 const chatModel = mongoose.model('Chat', chatSchema);
+passport.use(User.createStrategy());
 
-const obtainAllUsers = function() {
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+
+const obtainAllUsers = function () {
   userModel.find({});
 };
 
-const createUser = function(data) {
+const createUser = function (data) {
   userModel.create(data);
 };
 
 module.exports = {
   deckModel,
-  userModel,
+  User,
   chatModel,
   obtainAllUsers
 };

dc22a178227a18210b498f5ca86297b87232f0bd
zachmarullo <zachmarullo@gmail.com>
2022-11-05 19:03:20 -0500

    Env?

--- server/db/index.js
+++ server/db/index.js
@@ -54,7 +54,7 @@ const deckSchema = new Schema({
   cardId: String,
   userId: String
 });
-
+//hello
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,

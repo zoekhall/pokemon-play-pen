1a84cb6a327a598270b9fd10944557101c227483
codingMustache <jorcar1986@gmail.com>
2022-11-07 21:18:40 -0600

    Cleaned up Auth

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ const passport = require('passport');
 const { CurrentUser } = require('./routes/userRoutes.js');
 const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
 
-
-
 const app = express();
+
 app.use(session({
   secret: 'Our little secret.',
   resave: false,
   saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -23,14 +23,11 @@ const isLoggedIn = (req, res, next) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/login', express.static(path.join(__dirname, '../client/dist/login')));
-app.use('/home', isLoggedIn, express.static(path.join(__dirname, '../client/dist')));
-//renders static page
-// app.use(express.static(CLIENT_PATH));
+app.use('/home', isLoggedIn, express.static(path.join(CLIENT_PATH)));
 
 
 app.get('/', (req, res) => {
   res.redirect('/login');
-  // res.send('<a href="/auth/google"> Authenticate with Google</a>');
 });
 
 app.get('/auth/google',
@@ -44,26 +41,9 @@ app.get('/auth/google/callback',
   })
 );
 
-app.get('/home', isLoggedIn, (req, res) => {
-  //console.log(req);
-});
-
-app.get('/auth/failure', (req, res) => {
-  res.send('Something went wrong');
-});
-// app.get('/auth/google/callback',
-//   passport.authenticate('google', { failureRedirect: 'http://localhost:8080' }),
-//   function (req, res) {
-//     res.redirect('http://localhost:8080');
-//   });
-
-
-app.get('/logout', function (req, res) {
-  res.redirect('http://localhost:8080/');
-});
+app.get('/home', isLoggedIn, (req, res) => {});
 
 app.use('/api/current/user', CurrentUser);
-
 
 module.exports = {
   app,

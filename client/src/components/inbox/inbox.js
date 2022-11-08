bb5f3f23a9b8e8584c19eacbe60d8252ecf68c5f
codingMustache <jorcar1986@gmail.com>
2022-11-08 13:55:51 -0600

    refactored comp

--- client/src/components/inbox/inbox.js
+++ client/src/components/inbox/inbox.js
@@ -6,25 +6,14 @@ import axios from 'axios';
 
 const Inbox = () => {
 
-  const searchUsers = () =>{
-    axios.get('/api/user/findUser', {params: { name: 'tester'}})
-      .then(data => console.log(data));
-  };
 
-
-  const test = () =>{
-    console.log('Front hit!!!');
-    axios.get('/api/user', {params: { name: 'tester'}})
-      .then(data => console.log(data));
-  };
 
   return (
     <div>
       <form>
         <input type="text" />
-        <button onClick={ searchUsers }>SEARCH</button>
+        <button onClick={  }>SEARCH</button>
       </form>
-      <button onClick={test}>test</button>
     </div>
   );
 

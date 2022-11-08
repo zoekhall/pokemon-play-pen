808d57406587be5f00e670df3f095682af2316be
codingMustache <jorcar1986@gmail.com>
2022-11-08 11:55:32 -0600

    Feat: Returning User Search from Compo

--- client/src/components/inbox/compose.jsx
+++ client/src/components/inbox/compose.jsx
@@ -1,16 +1,20 @@
+import axios from 'axios';
 import React from 'react';
 
 
 const Compose = () => {
 // func to search for users to message
-
+  const searchUsers = () =>{
+    axios.get('/api/user/findUser', {params: { name: 'tester'}})
+      .then(data => console.log(data));
+  };
   return (
     <div>
       <form>
         <input type="text" />
-        <button>SEARCH</button>
+        <button onClick={searchUsers}>SEARCH</button>
       </form>
-        
+
     </div>
   );
 };

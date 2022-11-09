ca16feebd69c16078a457fb3161da06f53019fb4
Samuel <samlittellmds@gmail.com>
2022-11-08 20:39:43 -0600

    renders cards for collection  / button sends cards to marketplace / stores card in db

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(jsx|js)$/,
+        test: /\.(jsx|js|png)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',

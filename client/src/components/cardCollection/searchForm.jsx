7b099c2534d8249e4be702c9d9104165ba083ad3
Samuel <samlittellmds@gmail.com>
2022-11-09 16:55:35 -0600

    successfully searching cards by poke name from the back end request

--- client/src/components/cardCollection/searchForm.jsx
+++ client/src/components/cardCollection/searchForm.jsx
@@ -1,30 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const SearchForm = ( { setCards } ) => {
+const SearchForm = ({ setCards }) => {
 
-  // useEffect(() => {
-  //   axios.get('/api/deck', { params: { q: 'Charizard' } })
-  //     .then(data => console.log(data));
-  // }, []);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  const handleOnSubmit = async e => {
+    e.preventDefault();
+
+    setCards([]);
+
+    try {
+      const { data } = await axios.get('/api/deck', { params: { q: searchTerm } });
+      // console.log('data', data);
+      setCards(data);
+      setSearchTerm('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // this request is "just for show" - we could just have a blank page by default
   useEffect(() => {
     const getInitialCards = async () => {
       try {
-        const { data } = await axios.get('https://api.pokemontcg.io/v2/cards?pageSize=50');
+        const { data } = await axios.get('https://api.pokemontcg.io/v2/cards?pageSize=50',
+          {
+            headers: {
+              'api.pokemontcg.io-key': 'process.env.POKEMONTCG_API_KEY'
+            }
+          }
+        );
         setCards(data.data);
       } catch (error) {
-        console.error(err);
+        console.error(error);
       }
     };
     getInitialCards();
   }, []);
 
   return (
-    <div>
-      <h3>SearchForm</h3>
-    </div>
+    <form onSubmit={handleOnSubmit}>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button>
+          Search Cards
+        </button>
+      </div>
+    </form>
   );
 };
 
 export default SearchForm;
+
+
+
+// useEffect(() => {
+//   axios.get('/api/deck', { params: { q: 'Charizard' } })
+//     .then(data => console.log(data));
+// }, []);
+

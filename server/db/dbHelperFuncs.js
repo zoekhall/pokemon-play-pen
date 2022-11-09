9fe2089048c906462b3ae5ba77b37abaadafd2d1
codingMustache <jorcar1986@gmail.com>
2022-11-09 11:02:34 -0600

    Arrow Func fix

--- server/db/dbHelperFuncs.js
+++ server/db/dbHelperFuncs.js
@@ -1,24 +1,3 @@
-const db = require('mongoose');
-const { Deck, User, Chat } = require('./index.js');
-
-
-const obtainAllUsers = function () {
-  User.find({});
-};
-
-const createUser = (data) => {
-  User.create(data);
-};
-
-const findUser = user => {
-  User.findOne({firstName: user})
-    .then(data=> console.log(data));
-};
-
-const addCard = (card, cb) => {
-  Deck.create(card)
-    .catch(error => console.log(error));
-};
 
 module.exports = {
   obtainAllUsers,

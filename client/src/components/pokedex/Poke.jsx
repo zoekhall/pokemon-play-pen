e08620e5c13b13c35fe0cc7cd83e7305bcf6780a
zoekhall <zoe.k.hall@gmail.com>
2022-11-10 04:10:59 -0600

    add buttons to poke

--- client/src/components/pokedex/Poke.jsx
+++ client/src/components/pokedex/Poke.jsx
@@ -1,5 +1,28 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const PokeEntry = styled.div`
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  box-sizing: content-box;
+  width: 200px;
+  height: 50%;
+  padding: 15px;
+  margin: 25px;
+  display: inline-block;
+`;
+
+const PokeImg = styled.img`
+  position: center;
+`;
+
+const PokeName = styled.h4`
+  position: center;
+`;
+
+const Buttons = styled.div`
+  position: center;
+`;
 
 const Poke = ({selectPoke}) => {
   const [pokedex, setPokedex] = useState([]); //array of pokemon to be rendered 
@@ -17,10 +40,15 @@ const Poke = ({selectPoke}) => {
   return pokedex.map((pokemon, id) => {
     id++;
     return (
-      <div onClick={() => selectPoke(id)} >
-        {pokemon.name}
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}/>
-      </div>
+      <PokeEntry onClick={() => selectPoke(id)} >
+        <PokeImg src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
+        <PokeName>{pokemon.name}</PokeName>
+        <Buttons>
+          <button>Add { pokemon.name } as Favorite Pokemon</button>
+          <button>View {`${pokemon.name}'s`} Stats</button>
+          <button>View Cards Related to { pokemon.name }</button>
+        </Buttons>
+      </PokeEntry>
     );
   });
 };

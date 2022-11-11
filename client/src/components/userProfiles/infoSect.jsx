c6916025cb0011b5086ff14d275f0af6525cb36d
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2022-11-10 18:04:54 -0600

    Favorite pokemon functinality added

--- client/src/components/userProfiles/infoSect.jsx
+++ client/src/components/userProfiles/infoSect.jsx
@@ -9,7 +9,6 @@ import axios from 'axios';
 const InfoSect = ({ id }) => {
   const [profile, setProfile] = useState({});
   const [inputVals, setInputVals] = useState({});
-  const [pokemon, setPokemon] = useState({});
 
   const retriveIdData = () => { // get the profile object data from db
     axios.get('/api/user/current') // then just return YOU the current user data
@@ -22,12 +21,6 @@ const InfoSect = ({ id }) => {
         });
       })
       .catch(err => console.log(err, 'ERROR ON GET CURRENT USER'));
-  };
-
-  const getFavPokemonData = () => {
-    axios.get('api/user/current/pokemon')
-      .then(data => console.log('works'))
-      .catch(err => console.log(err, 'pokemon err'));
   };
 
   const changeUsername = (newName) => { // the inputted name will be sent to the database to be edited
@@ -45,7 +38,7 @@ const InfoSect = ({ id }) => {
   const handleClick = () => { // update the username and description witht he incomming user input
     if (userRef.current.value.length !== 0) { // if the text field is not empty change the username
       changeUsername(userRef.current.value);
-    } 
+    }
 
     if (descRef.current.value.length !== 0) {
       changeDescription(descRef.current.value); // if the description field is not empty change the description
@@ -57,10 +50,8 @@ const InfoSect = ({ id }) => {
   }, []);
 
 
-
   const userRef = useRef(null); // the refrence for the username edit text 
   const descRef = useRef(null); // the refrence for the description edit text
-
   return (
     <div>
       <img alt={profile.firstName} width='100px' src={profile.avatar} referrerpolicy="no-referrer" /> <br />
@@ -68,14 +59,25 @@ const InfoSect = ({ id }) => {
       <button onClick={() => setInputVals(() => ({ clicked: true }))}>Edit Profile</button> <br />
 
       Your Username:  {inputVals.clicked ?
-        <div><input ref={userRef}/> </div> :
+        <div><input ref={userRef} /> </div> :
         <div>{profile.username}</div>} <br />
 
       Your Description: {inputVals.clicked ?
-        <div><input ref={descRef}/></div> :
-        <div>{profile.description}</div>}
+        <div><input ref={descRef} /></div> :
+        <div>{profile.description}</div>} <br />
 
-      Your Favorite Pokemon: 
+      Your Favorite Pokemon:
+      <div>
+        <img src={profile.favPokemonImage} alt={profile.favPokemonName} width='90px' referrerpolicy='no-referrer' /> <br />
+        <h2>
+          {profile.favPokemonName}
+        </h2>  <br />
+        <ul>
+          <li>{profile.favPokemonType1}</li>
+          {profile.favPokemonType2 ? <li>{profile.favPokemonType2}</li> : <></>}
+        </ul>
+      </div>
+
 
       {inputVals.clicked ?
         <div><button onClick={() => {

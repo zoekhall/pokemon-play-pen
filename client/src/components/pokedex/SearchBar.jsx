936cae11f1590c8f8d12c8248116ec12b8ceac1a
zoekhall <zoe.k.hall@gmail.com>
2022-11-11 15:28:43 -0600

    added functionality to pokedex - add card to marketplace

--- client/src/components/pokedex/SearchBar.jsx
+++ client/src/components/pokedex/SearchBar.jsx
@@ -2,8 +2,8 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 const SearchBar = () => {
-  const [searchTerm, setSearchTerm] = useState(''); //state to hold search term inputted
-  const [results, setResults] = useState([]); //state to hold search results
+  // const [searchTerm, setSearchTerm] = useState(''); //state to hold search term inputted
+  // const [results, setResults] = useState([]); //state to hold search results
 
 
   return (
